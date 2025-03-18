cd786cf0796d4c6ab356c6ce2ee99559aef5bb8f
Giorgio Kalmund <giorgio@kalmund.com>
2025-03-18 17:56:47 +0100

    feat: raw jade and jade, start of blocks

--- src/main/java/com/mgmstudios/projectj/ProjectJ.java
+++ src/main/java/com/mgmstudios/projectj/ProjectJ.java
@@ -1,5 +1,8 @@
 package com.mgmstudios.projectj;
 
+import com.mgmstudios.projectj.block.ModBlocks;
+import com.mgmstudios.projectj.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -38,6 +41,9 @@ public class ProjectJ
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -52,7 +58,11 @@ public class ProjectJ
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.JADE);
+            event.accept(ModItems.RAW_JADE);
+            event.accept(ModBlocks.ADOBE_BLOCK);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/mgmstudios/projectj/item/ModItems.java
@@ -0,0 +1,20 @@
+package com.mgmstudios.projectj.item;
+
+import com.mgmstudios.projectj.ProjectJ;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ProjectJ.MOD_ID);
+
+    public static final DeferredItem<Item> JADE = ITEMS.registerItem("jade",
+            Item::new, new Item.Properties());
+    public static final DeferredItem<Item> RAW_JADE = ITEMS.registerItem("raw_jade",
+            Item::new, new Item.Properties());
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
