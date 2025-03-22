62674281dd0f651136c8bb608af755c5591d3b82
Giorgio Kalmund <147076723+GiorgioKalmund@users.noreply.github.com>
2025-03-22 22:51:30 +0100

    feat: lit basic olmet head gives speed effect for 10 seconds when right clicking

--- src/main/java/com/mgmstudios/projectj/block/custom/OlmecHeadBlock.java
+++ src/main/java/com/mgmstudios/projectj/block/custom/OlmecHeadBlock.java
@@ -8,9 +8,14 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -19,6 +24,7 @@ import net.minecraft.world.level.block.RedstoneLampBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
@@ -35,6 +41,16 @@ public class OlmecHeadBlock extends RedstoneLampBlock {
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return SHAPE;
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+        if (state.getValue(LIT) && level instanceof ServerLevel serverLevel){
+            player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 10 * 20));
+            serverLevel.playSound(null, pos, SoundEvents.WANDERING_TRADER_DRINK_POTION, SoundSource.BLOCKS, 1f, 1f);
+            return InteractionResult.SUCCESS_SERVER;
+        }
+        return super.useWithoutItem(state, level, pos, player, hitResult);
     }
 
     @Override
