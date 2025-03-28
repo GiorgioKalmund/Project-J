c4128b7930dac8017ceec9e8926527ec292b5769
Giorgio Kalmund <giorgio@kalmund.com>
2025-03-28 14:28:59 +0100

    feat: start of mod log block

---
+++ src/main/java/com/mgmstudios/projectj/block/custom/StrippableModLogBlock.java
@@ -0,0 +1,27 @@
+﻿package com.mgmstudios.projectj.block.custom;
+
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.ItemAbility;
+import org.jetbrains.annotations.Nullable;
+
+public class StrippableModLogBlock extends RotatedPillarBlock {
+
+    private final Block strippedBlock;
+
+    public StrippableModLogBlock(Properties p_55926_, Block strippedBlock) {
+        super(p_55926_);
+        this.strippedBlock = strippedBlock;
+    }
+
+    @Override
+    public @Nullable BlockState getToolModifiedState(BlockState state, UseOnContext context, ItemAbility itemAbility, boolean simulate) {
+        if (context.getItemInHand().getItem() instanceof AxeItem){
+            return strippedBlock.defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+        }
+        return super.getToolModifiedState(state, context, itemAbility, simulate);
+    }
+}
