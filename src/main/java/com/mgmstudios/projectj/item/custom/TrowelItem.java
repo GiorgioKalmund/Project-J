45cc5d30c7b836b1b6e80bbd3d95b43e07310f11
Giorgio Kalmund <giorgio@kalmund.com>
2025-03-29 18:21:20 +0100

    feat: strart of trowel item

---
+++ src/main/java/com/mgmstudios/projectj/item/custom/TrowelItem.java
@@ -0,0 +1,114 @@
+﻿package com.mgmstudios.projectj.item.custom;
+
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.client.multiplayer.chat.report.ReportEnvironment;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.BlockItemStateProperties;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class TrowelItem extends BlockItem {
+
+
+    public TrowelItem(Properties properties) {
+        super(Blocks.DIAMOND_BLOCK, properties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        if (level instanceof ServerLevel serverLevel && context.getPlayer() != null){
+            BlockHitResult hitResult = new BlockHitResult(context.getClickLocation(), context.getHorizontalDirection(), context.getClickedPos(), context.isInside());
+            BlockPlaceContext blockPlaceContext = new BlockPlaceContext(context.getPlayer(), context.getHand(), context.getItemInHand(), hitResult);
+        }
+        return super.useOn(context);
+    }
+
+    public InteractionResult place(BlockPlaceContext context, ItemStack stackToPlace) {
+        if (!this.getBlock().isEnabled(context.getLevel().enabledFeatures())) {
+            return InteractionResult.FAIL;
+        } else if (!context.canPlace()) {
+            return InteractionResult.FAIL;
+        } else {
+            BlockPlaceContext blockplacecontext = this.updatePlacementContext(context);
+            if (blockplacecontext == null) {
+                return InteractionResult.FAIL;
+            } else {
+                BlockState blockstate = this.getPlacementState(blockplacecontext);
+                if (blockstate == null) {
+                    return InteractionResult.FAIL;
+                } else if (!this.placeBlock(blockplacecontext, blockstate)) {
+                    return InteractionResult.FAIL;
+                } else {
+                    BlockPos blockpos = blockplacecontext.getClickedPos();
+                    Level level = blockplacecontext.getLevel();
+                    Player player = blockplacecontext.getPlayer();
+                    ItemStack itemstack = stackToPlace;
+                    BlockState blockstate1 = level.getBlockState(blockpos);
+                    if (blockstate1.is(blockstate.getBlock())) {
+                        blockstate1 = updateBlockStateFromTag(blockpos, level, itemstack, blockstate1);
+                        this.updateCustomBlockEntityTag(blockpos, level, player, itemstack, blockstate1);
+                        updateBlockEntityComponents(level, blockpos, itemstack);
+                        blockstate1.getBlock().setPlacedBy(level, blockpos, blockstate1, player, itemstack);
+                        if (player instanceof ServerPlayer) {
+                            CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayer)player, blockpos, itemstack);
+                        }
+                    }
+
+                    SoundType soundtype = blockstate1.getSoundType(level, blockpos, context.getPlayer());
+                    level.playSound(
+                            player,
+                            blockpos,
+                            this.getPlaceSound(blockstate1, level, blockpos, context.getPlayer()),
+                            SoundSource.BLOCKS,
+                            (soundtype.getVolume() + 1.0F) / 2.0F,
+                            soundtype.getPitch() * 0.8F
+                    );
+                    level.gameEvent(GameEvent.BLOCK_PLACE, blockpos, GameEvent.Context.of(player, blockstate1));
+                    itemstack.consume(1, player);
+                    return InteractionResult.SUCCESS;
+                }
+            }
+        }
+    }
+
+    private BlockState updateBlockStateFromTag(BlockPos pos, Level level, ItemStack stack, BlockState state) {
+        BlockItemStateProperties blockitemstateproperties = stack.getOrDefault(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY);
+        if (blockitemstateproperties.isEmpty()) {
+            return state;
+        } else {
+            BlockState blockstate = blockitemstateproperties.apply(state);
+            if (blockstate != state) {
+                level.setBlock(pos, blockstate, 2);
+            }
+
+            return blockstate;
+        }
+    }
+
+    private static void updateBlockEntityComponents(Level level, BlockPos poa, ItemStack stack) {
+        BlockEntity blockentity = level.getBlockEntity(poa);
+        if (blockentity != null) {
+            blockentity.applyComponentsFromItemStack(stack);
+            blockentity.setChanged();
+        }
+    }
+}
