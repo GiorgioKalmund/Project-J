71e93e71b9e3acb3e76259342fe229aff3b4b1ee
Giorgio Kalmund <giorgio@kalmund.com>
2025-04-06 13:51:13 +0200

    idea: custom structure generation

---
+++ src/main/java/com/mgmstudios/projectj/worldgen/ModStructureSets.java
@@ -0,0 +1,30 @@
+package com.mgmstudios.projectj.worldgen;
+
+import com.mgmstudios.projectj.ProjectJ;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
+import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadStructurePlacement;
+import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadType;
+
+public class ModStructureSets {
+
+    public static final ResourceKey<StructureSet> BIG_OLMEC_HEADS = createKey("big_olmec_heads");
+
+    private static ResourceKey<StructureSet> createKey(String name) {
+        return ResourceKey.create(Registries.STRUCTURE_SET, ResourceLocation.fromNamespaceAndPath(ProjectJ.MOD_ID, name));
+    }
+
+    public static void bootstrap(BootstrapContext<StructureSet> context) {
+        HolderGetter<Structure> structures = context.lookup(Registries.STRUCTURE);
+
+        context.register(BIG_OLMEC_HEADS, new StructureSet(structures.getOrThrow(ModStructures.BIG_OLMEC_HEAD),
+                new RandomSpreadStructurePlacement(10, 5, RandomSpreadType.LINEAR, 2738116)));
+    }
+
+
+    }
---
+++ src/main/java/com/mgmstudios/projectj/worldgen/ModStructures.java
@@ -0,0 +1,40 @@
+package com.mgmstudios.projectj.worldgen;
+
+import com.mgmstudios.projectj.ProjectJ;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.structures.DesertPyramidStructure;
+import net.minecraft.world.level.levelgen.structure.structures.JungleTempleStructure;
+import net.neoforged.neoforge.common.world.StructureSettingsBuilder;
+
+import java.util.function.Function;
+
+public class ModStructures {
+
+    public static final ResourceKey<Structure> BIG_OLMEC_HEAD = createKey("big_olmec_head");
+
+    public static void bootstrap(BootstrapContext<Structure> context) {
+        HolderGetter<Biome> holdergetter = context.lookup(Registries.BIOME);
+        HolderGetter<StructureTemplatePool> templatePools = context.lookup(Registries.TEMPLATE_POOL);
+
+        context.register(BIG_OLMEC_HEAD, new DesertPyramidStructure(new Structure.StructureSettings.Builder(
+                holdergetter.getOrThrow(BiomeTags.HAS_SWAMP_HUT))
+                .generationStep(GenerationStep.Decoration.SURFACE_STRUCTURES)
+                .build()
+        ));
+
+    }
+
+    private static ResourceKey<Structure> createKey(String name) {
+        return ResourceKey.create(Registries.STRUCTURE, ResourceLocation.fromNamespaceAndPath(ProjectJ.MOD_ID, name));
+    }
+}
