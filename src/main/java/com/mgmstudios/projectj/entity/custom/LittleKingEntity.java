2462e7ac396073679123ef344c813feff870b0f3
Giorgio Kalmund <giorgio@kalmund.com>
2025-04-14 21:01:25 +0200

    feat: little king texture, model and animation

--- src/main/java/com/mgmstudios/projectj/entity/custom/LittleKingEntity.java
+++ src/main/java/com/mgmstudios/projectj/entity/custom/LittleKingEntity.java
@@ -6,24 +6,48 @@
 public class little_king<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
 	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "little_king"), "main");
-	private final ModelPart bb_main;
+	private final ModelPart leftleg;
+	private final ModelPart leftarm;
+	private final ModelPart lowerbody;
+	private final ModelPart head;
+	private final ModelPart rightarm;
+	private final ModelPart rightleg;
+	private final ModelPart backpack;
 
 	public little_king(ModelPart root) {
-		this.bb_main = root.getChild("bb_main");
+		this.leftleg = root.getChild("leftleg");
+		this.leftarm = root.getChild("leftarm");
+		this.lowerbody = root.getChild("lowerbody");
+		this.head = root.getChild("head");
+		this.rightarm = root.getChild("rightarm");
+		this.rightleg = root.getChild("rightleg");
+		this.backpack = root.getChild("backpack");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -3.0F, -1.0F, 2.0F, 3.0F, 2.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 0).mirror().addBox(3.0F, -11.0F, -1.0F, 2.0F, 6.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false)
-		.texOffs(0, 0).addBox(-5.0F, -11.0F, -1.0F, 2.0F, 6.0F, 2.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 0).mirror().addBox(1.0F, -3.0F, -1.0F, 2.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false)
-		.texOffs(-2, -2).addBox(-3.0F, -16.0F, -2.0F, 6.0F, 13.0F, 4.0F, new CubeDeformation(0.0F))
-		.texOffs(-1, 2).addBox(-6.0F, -20.0F, 0.0F, 12.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition leftleg = partdefinition.addOrReplaceChild("leftleg", CubeListBuilder.create().texOffs(28, 9).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 21.0F, 0.0F));
 
-		return LayerDefinition.create(meshdefinition, 16, 16);
+		PartDefinition leftarm = partdefinition.addOrReplaceChild("leftarm", CubeListBuilder.create().texOffs(20, 17).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 6.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(-4.0F, 14.0F, 0.0F));
+
+		PartDefinition lowerbody = partdefinition.addOrReplaceChild("lowerbody", CubeListBuilder.create().texOffs(0, 19).addBox(-1.0F, -13.0F, -2.0F, 6.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 28.0F, 0.0F));
+
+		PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 8).addBox(-1.0F, -15.0F, -6.0F, 6.0F, 7.0F, 4.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 0).addBox(-4.0F, -19.0F, -4.0F, 12.0F, 8.0F, 0.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 29).addBox(1.0F, -11.0F, -2.0F, 2.0F, 3.0F, 1.0F, new CubeDeformation(0.0F))
+		.texOffs(20, 8).addBox(-3.0F, -20.0F, -4.0F, 10.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 23.0F, 4.0F));
+
+		PartDefinition rightarm = partdefinition.addOrReplaceChild("rightarm", CubeListBuilder.create().texOffs(20, 9).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 6.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(4.0F, 14.0F, 0.0F));
+
+		PartDefinition rightleg = partdefinition.addOrReplaceChild("rightleg", CubeListBuilder.create().texOffs(28, 14).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, 21.0F, 0.0F));
+
+		PartDefinition backpack = partdefinition.addOrReplaceChild("backpack", CubeListBuilder.create().texOffs(24, 0).addBox(-2.0F, -2.0F, 1.0F, 4.0F, 5.0F, 1.0F, new CubeDeformation(0.0F))
+		.texOffs(28, 19).addBox(-2.0F, -4.0F, 1.0F, 4.0F, 2.0F, 0.0F, new CubeDeformation(0.0F))
+		.texOffs(20, 25).addBox(-2.0F, -1.0F, 0.0F, 4.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 17.0F, -4.0F));
+
+		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
 
 	@Override
@@ -33,6 +57,12 @@ public class little_king<T extends Entity> extends EntityModel<T> {
 
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		bb_main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		leftleg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		leftarm.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		lowerbody.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		head.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		rightarm.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		rightleg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		backpack.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 }
